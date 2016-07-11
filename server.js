6e0c5073e1664264210005ca19c6ff89a8d2d9a4
SmileAwhile <cjdunn11@gmail.com>
2016-07-11 11:48:48 -0700

    testing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express();
+var parser = require('ua-parser-js');
+var locale = require('locale');
 
 app.get('/', function (req, res) {
-  res.end("hello");
+
+    var ua = parser(req.headers['user-agent']);
+    var ip = req.ip;
+    var loc = req.headers["accept-language"];
+    var obj = { "ipaddress": ip, "language": loc, "software": ua.browser.name + ", " + ua.os.name + " " + ua.os.version};
+    res.end(JSON.stringify(obj, null, '  '));
+
 });
 
 var port = process.env.PORT || 3000;
